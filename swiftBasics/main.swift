47366c5db9d067b7a25989fc9d4304842b528552
Josh <jdbb7d@mail.missouri.edu>
2019-09-04 18:10:48 -0500

    Some functions implemented

--- swiftBasics/main.swift
+++ swiftBasics/main.swift
@@ -21,3 +21,32 @@ var choice: Int = 2
 let integral: Character = "\u{222B}"
 let greeting: String = "Hello"
 var name: String = "Karen"
+
+if sample1 == sample2 {
+    print("The samples are equal.")
+} else {
+    print("The samples are not equal.")
+}
+
+if heartRate > 40 && heartRate <= 80 {
+    print("Heart rate is normal.")
+} else {
+    print("Heart rate is not normal.")
+}
+
+if deposits >= 100000000 {
+    print("You are exceedingly wealthy.")
+} else {
+    print("Sorry you are so poor.")
+}
+
+var force = mass * acceleration
+print("force = \(force)")
+
+print("\(distance) is the distance.")
+
+if lost && expensive {
+    print("I am really sorry! I will get the manager.")
+} else if lost && !expensive {
+    print("Here is coupon for 10% off.")
+}
