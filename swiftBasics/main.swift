4b2c2b9252bd9c5e8bbb9d82918889988108db2d
Josh <jdbb7d@mail.missouri.edu>
2019-09-04 17:36:23 -0500

    Initial Commit

---
+++ swiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftBasics
+//
+//  Created by Joshua Brooks on 9/4/19.
+//  Copyright © 2019 Joshua Brooks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
